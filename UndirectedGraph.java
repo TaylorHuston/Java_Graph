f511782bf500fa797a9788d154972db1f9f15000
Taylor Huston <taylor.a.huston@gmail.com>
2015-07-13 20:13:37 -0700

    Add basic structure of graph

---
+++ UndirectedGraph.java
@@ -0,0 +1,45 @@
+/**
+ Undirected Graph implementation
+ Based on Algorithms, 4th Ed by Robert Sedgewick | Kevin Wayne
+ */
+public class UndirectedGraph {
+
+    //Constructor with a pre-supplied number if vertices
+    UndirectedGraph(int V) {
+
+    }
+
+//    //Constructor with a graph built from input stream
+//    UndirectedGraph(In in) {
+//
+//    }
+//
+//    //Number of vertices
+//    int V() {
+//
+//    }
+//
+//    //Number of edges
+//    int E() {
+//
+//    }
+//
+//    //Connect two vertices (first to second)
+//    void AddEdge(int first, int second) {
+//
+//    }
+//
+//    //vertices adjacent to vertex
+//    Interable<Integer> adv(int vertex) {
+//
+//    }
+//
+//    String toSting() {
+//
+//    }
+
+    //Sample Client code operations on an Undirected Graph
+    public static void main (String args[]) {
+
+    }
+}
