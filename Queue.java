9adfc18014b7b14972979d7b3dd4b4594e5a008c
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-14 21:21:21 -0700

    Implement Strongly Connected Components for Directed Graphs

--- Queue.java
+++ Queue.java
@@ -1,59 +1,171 @@
-/*
-Basic Queue using a Linked List 
-Based on Section 1.3 of Algorithms, 4th Edition
-*/
+/******************************************************************************
+ *  Compilation:  javac Queue.java
+ *  Execution:    java Queue < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *  Data files:   http://algs4.cs.princeton.edu/13stacks/tobe.txt  
+ *
+ *  A generic queue, implemented using a linked list.
+ *
+ *  % java Queue < tobe.txt 
+ *  to be or not to be (2 left on queue)
+ *
+ ******************************************************************************/
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Queue<T> implements Iterable<T> {
+/**
+ *  The <tt>Queue</tt> class represents a first-in-first-out (FIFO)
+ *  queue of generic items.
+ *  It supports the usual <em>enqueue</em> and <em>dequeue</em>
+ *  operations, along with methods for peeking at the first item,
+ *  testing if the queue is empty, and iterating through
+ *  the items in FIFO order.
+ *  <p>
+ *  This implementation uses a singly-linked list with a static nested class for
+ *  linked-list nodes. See {@link LinkedQueue} for the version from the
+ *  textbook that uses a non-static nested class.
+ *  The <em>enqueue</em>, <em>dequeue</em>, <em>peek</em>, <em>size</em>, and <em>is-empty</em>
+ *  operations all take constant time in the worst case.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ *
+ *  @param <Item> the generic type of an item in this bag
+ */
+public class Queue<Item> implements Iterable<Item> {
+    private int N;               // number of elements on queue
+    private Node<Item> first;    // beginning of queue
+    private Node<Item> last;     // end of queue
 
-    //LinkedList that holds the Queue
-    private LinkedList<T> queueList;
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
 
-    //Constructor
+    /**
+     * Initializes an empty queue.
+     */
     public Queue() {
-        queueList = new LinkedList<T>();
+        first = null;
+        last  = null;
+        N = 0;
     }
 
-    //Adds item to front of queue
-    public void enqueue(T toAdd) {
-        queueList.addToFront(toAdd);
-    }
-
-    //Gets and removes item from end of queue
-    public T dequeue() {
-        return queueList.removeFromEnd();
-    }
-
+    /**
+     * Returns true if this queue is empty.
+     *
+     * @return <tt>true</tt> if this queue is empty; <tt>false</tt> otherwise
+     */
     public boolean isEmpty() {
-        return queueList.isEmpty();
+        return first == null;
     }
 
-    //Iterator
-    public Iterator<T> iterator() {
-        return queueList.iterator();
+    /**
+     * Returns the number of items in this queue.
+     *
+     * @return the number of items in this queue
+     */
+    public int size() {
+        return N;     
     }
 
+    /**
+     * Returns the item least recently added to this queue.
+     *
+     * @return the item least recently added to this queue
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return first.item;
+    }
+
+    /**
+     * Adds the item to this queue.
+     *
+     * @param  item the item to add
+     */
+    public void enqueue(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else           oldlast.next = last;
+        N++;
+    }
+
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     *
+     * @return the item on this queue that was least recently added
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = first.item;
+        first = first.next;
+        N--;
+        if (isEmpty()) last = null;   // to avoid loitering
+        return item;
+    }
+
+    /**
+     * Returns a string representation of this queue.
+     *
+     * @return the sequence of items in FIFO order, separated by spaces
+     */
     public String toString() {
-        String s = "";
+        StringBuilder s = new StringBuilder();
+        for (Item item : this)
+            s.append(item + " ");
+        return s.toString();
+    } 
 
-        for (T i : queueList) {
-            s += i + " ";
+    /**
+     * Returns an iterator that iterates over the items in this queue in FIFO order.
+     *
+     * @return an iterator that iterates over the items in this queue in FIFO order
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);  
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
         }
 
-        return s;
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
     }
 
-    //Test client for Queue
+
+    /**
+     * Unit tests the <tt>Queue</tt> data type.
+     */
     public static void main(String[] args) {
-        Queue myQueue = new Queue();
-
-        myQueue.enqueue("Test");
-        myQueue.enqueue("Test2");
-        StdOut.println(myQueue.dequeue());
-        StdOut.println(myQueue.dequeue());
-        myQueue.enqueue("Test3");
-        StdOut.println(myQueue.dequeue());
-
-    } //End main
-} //End Queue
+        Queue<String> q = new Queue<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) q.enqueue(item);
+            else if (!q.isEmpty()) StdOut.print(q.dequeue() + " ");
+        }
+        StdOut.println("(" + q.size() + " left on queue)");
+    }
+}
